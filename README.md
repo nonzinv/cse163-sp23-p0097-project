b530a2c39335f9457af2fde3debe4a7239081d77
nonzinv <nonzinv@uw.edu>
2023-05-25 13:54:56 -0700

    readme added + changes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# cse163-sp23-p0097-project
+Video Game Sales Analysis by Non Pronanun & Sirak Yohannes
+
+**Research Questions and Results**
+1. Are video game sales affected by the COVID19 pandemic?
+   - Yes, the data seems to be backing this.
+   - Based on the dataset, we are projected to have 55 Million Sales in 2020, but the real number was 140 Million sales.
+2. Are video game genres evolving, or do they remain constant?
+   - The popularity of genres remains pretty constant with minor variations.
+   - The four most popular genres are platform, shooter, action, and role playing -- though their order of popularity are changing.
--- project.py
+++ project.py
@@ -1,11 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
 from plotly.subplots import make_subplots
-
 
 
 def predict_in_2020(df):
@@ -13,13 +10,10 @@ def predict_in_2020(df):
     df = df[['Global_Sales', 'NA_Sales', 'EU_Sales', 'Year', 'Genre']]
     agg_functions = {'Global_Sales': 'sum', 'NA_Sales': 'sum', 'EU_Sales': 'sum'}
     df = df.groupby(['Year', 'Genre']).aggregate(agg_functions).reset_index()
-
     model = LinearRegression()
     model.fit(df[['Year']], df['Global_Sales'])
-
     sales_2020 = model.predict([[2020]])
     actual_sales_2020 = 140.4
-
     fig = px.scatter(df, x='Year', y='Global_Sales', opacity=0.65,
                      trendline='ols', trendline_color_override='green',
                      labels={
@@ -30,6 +24,7 @@ def predict_in_2020(df):
     fig.add_scatter(x=[2020], y=[actual_sales_2020], mode='markers', name='Actual Sales in 2020')
     fig.update_traces(marker=dict(size=8))
     fig.write_image("images/predict_2020.png")
+
 
 def genre_trends(df):
     df = df[['Genre', 'Year', 'Global_Sales']]
@@ -42,7 +37,7 @@ def genre_trends(df):
     values_2010 = data_2010['Global_Sales']
     values_2005 = data_2005['Global_Sales']
     values_2015 = data_2015['Global_Sales']
-    specs = [[{'type':'domain'}, {'type':'domain'}], [{'type':'domain'}, {'type':'domain'}]]
+    specs = [[{'type': 'domain'}, {'type': 'domain'}], [{'type': 'domain'}, {'type': 'domain'}]]
     fig = make_subplots(rows=2, cols=2, vertical_spacing=0.1, horizontal_spacing=0.1,
                         specs=specs, subplot_titles=['2000', '2005', '2010', '2015'])
     fig.add_trace(go.Pie(labels=labels, values=values_2000, scalegroup='one'), 1, 1)
@@ -51,7 +46,6 @@ def genre_trends(df):
     fig.add_trace(go.Pie(labels=labels, values=values_2015, scalegroup='one'), 2, 2)
     fig.update_layout(title="Distribution of Video Game Genres between 2000 and 2015 \
                       <br><sup>The size of the charts represents the relative volume of sales</sup>")
-
     fig.write_image("images/genre_trends.png")
 
 
@@ -59,6 +53,7 @@ def main() -> None:
     game_sales = pd.read_csv('vgsales.csv')
     predict_in_2020(game_sales)
     genre_trends(game_sales)
+
 
 if __name__ == '__main__':
     main()
