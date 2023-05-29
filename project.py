4b8bf9821c94cffba4777500efdd50e084c4943e
nonzinv <nonzinv@uw.edu>
2023-05-28 17:25:36 -0700

    added report

--- project.py
+++ project.py
@@ -1,14 +1,26 @@
+"""
+CSE163 Final Project. Non Pronanun & Sirak Yohannes
+This file is responsible for the code of this project, it contains the
+algorithms behind each of the research questions -- to find how the
+COVID19 pandemic affected video game sales and the market share of each
+video game genres.
+"""
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
 from plotly.subplots import make_subplots
 
-
+"""
+This method takes in a dataframe, it filters out the dataframes for the number
+of sales and genres, then it will predict the number of sales in 2020 and put
+that against the actual number of sales in 2020.
+"""
 def predict_in_2020(df):
     df = df[df['Year'] < 2015]
     df = df[['Global_Sales', 'NA_Sales', 'EU_Sales', 'Year', 'Genre']]
-    agg_functions = {'Global_Sales': 'sum', 'NA_Sales': 'sum', 'EU_Sales': 'sum'}
+    agg_functions = {'Global_Sales': 'sum', 'NA_Sales': 'sum',
+                     'EU_Sales': 'sum'}
     df = df.groupby(['Year', 'Genre']).aggregate(agg_functions).reset_index()
     model = LinearRegression()
     model.fit(df[['Year']], df['Global_Sales'])
@@ -20,12 +32,20 @@ def predict_in_2020(df):
                          'Global_Sales' : 'Sales in Millions',
                      },
                      title="Are Video Game Sales Affected by the Pandemic?")
-    fig.add_scatter(x=[2020], y=[sales_2020[0]], mode='markers', name='Predicted Sales in 2020')
-    fig.add_scatter(x=[2020], y=[actual_sales_2020], mode='markers', name='Actual Sales in 2020')
+    fig.add_scatter(x=[2020], y=[sales_2020[0]], mode='markers',
+                    name='Predicted Sales in 2020')
+    fig.add_scatter(x=[2020], y=[actual_sales_2020], mode='markers',
+                    name='Actual Sales in 2020')
     fig.update_traces(marker=dict(size=8))
     fig.write_image("images/predict_2020.png")
 
 
+"""
+This method takes in the video game dataframe and it will plot the
+statistics of sales by genre of video games between 2000 and 2015,
+by 5 year intervals. The size of each of the pie charts are also
+dependant on the number of sales in that year (smaller=less, etc.)
+"""
 def genre_trends(df):
     df = df[['Genre', 'Year', 'Global_Sales']]
     data_2000 = df[df['Year'] == 2000]
@@ -37,15 +57,20 @@ def genre_trends(df):
     values_2010 = data_2010['Global_Sales']
     values_2005 = data_2005['Global_Sales']
     values_2015 = data_2015['Global_Sales']
-    specs = [[{'type': 'domain'}, {'type': 'domain'}], [{'type': 'domain'}, {'type': 'domain'}]]
-    fig = make_subplots(rows=2, cols=2, vertical_spacing=0.1, horizontal_spacing=0.1,
-                        specs=specs, subplot_titles=['2000', '2005', '2010', '2015'])
-    fig.add_trace(go.Pie(labels=labels, values=values_2000, scalegroup='one'), 1, 1)
-    fig.add_trace(go.Pie(labels=labels, values=values_2010, scalegroup='one'), 2, 1)
-    fig.add_trace(go.Pie(labels=labels, values=values_2005, scalegroup='one'), 1, 2)
-    fig.add_trace(go.Pie(labels=labels, values=values_2015, scalegroup='one'), 2, 2)
-    fig.update_layout(title="Distribution of Video Game Genres between 2000 and 2015 \
-                      <br><sup>The size of the charts represents the relative volume of sales</sup>")
+    specs = [[{'type': 'domain'}, {'type': 'domain'}], [{'type': 'domain'},
+                                                        {'type': 'domain'}]]
+    fig = make_subplots(rows=2, cols=2, vertical_spacing=0.1,
+                        horizontal_spacing=0.1, specs=specs,
+                        subplot_titles=['2000', '2005', '2010', '2015'])
+    fig.add_trace(go.Pie(labels=labels, values=values_2000,
+                         scalegroup='one'), 1, 1)
+    fig.add_trace(go.Pie(labels=labels, values=values_2010,
+                         scalegroup='one'), 2, 1)
+    fig.add_trace(go.Pie(labels=labels, values=values_2005,
+                         scalegroup='one'), 1, 2)
+    fig.add_trace(go.Pie(labels=labels, values=values_2015,
+                         scalegroup='one'), 2, 2)
+    fig.update_layout(title="Distribution of Video Game Genres between 2000 and 2015")
     fig.write_image("images/genre_trends.png")
 
 
